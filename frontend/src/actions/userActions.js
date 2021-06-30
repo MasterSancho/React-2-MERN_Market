607fcb066e2785d624466d3a74d8a32ca85147d3
Alex M <1maltsevalex@gmail.com>
2021-06-30 12:35:23 +0300

    45. Show User In Navbar & Logout

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -3,6 +3,7 @@ import {
  USER_LOGIN_REQUEST,
  USER_LOGIN_SUCCESS,
  USER_LOGIN_FAIL,
+ USER_LOGOUT,
 } from '../constants/userConstants';
 
 export const login = (email, password) => async (dispatch) => {
@@ -25,4 +26,10 @@ export const login = (email, password) => async (dispatch) => {
      : error.message,
   });
  }
+};
+
+export const logout = () => (dispatch) => {
+ localStorage.removeItem('userInfo');
+ localStorage.removeItem('cartItems');
+ dispatch({ type: USER_LOGOUT });
 };
