c7185482f1d5a876905be3aa5540568b223e3dca
Alex M <1maltsevalex@gmail.com>
2021-07-07 08:23:37 +0300

    88. Custom Page Titles & Meta

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -13,14 +13,12 @@ import {
 import Rating from '../components/Rating';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
+import Meta from '../components/Meta';
 import {
  listProductDetails,
  createProductReview,
 } from '../actions/productActions';
-import {
- PRODUCT_CREATE_RESET,
- PRODUCT_CREATE_REVIEW_RESET,
-} from '../constants/productConstants';
+import { PRODUCT_CREATE_REVIEW_RESET } from '../constants/productConstants';
 
 const ProductScreen = ({ history, match }) => {
  const [qty, setQty] = useState(1);
@@ -74,6 +72,7 @@ const ProductScreen = ({ history, match }) => {
     <Message variant='danger'>{error}</Message>
    ) : (
     <>
+     <Meta title={product.name} />
      <Row>
       <Col md={6}>
        <Image src={product.image} alt={product.name} fluid />
